9c92f5f2ae1818568efcca4098110745f10ed714
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-28 17:41:54 +0100

    sidebar

---
+++ src/lib/components/SideBar.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+    import { Menu, Home, Globe2, Cog } from "lucide-svelte";
+    import { browser } from "$app/environment";
+
+    let location = "";
+    let phone = false;
+
+    if (browser) {
+        location = window.location.pathname;
+
+        window.addEventListener("resize", () => {
+            phone = window.innerWidth < 768;
+        });
+    }
+
+    let expanded = false;
+</script>
+
+<nav
+    class={`bg-ctp-mantle h-full border-r border-r-ctp-surface0 p-1 py-5 flex flex-col items-center transition-all duration-300 ${
+        expanded ? (phone ? "w-32" : "w-36") : "w-16"
+    }`}
+>
+    <h1 class="text-lg font-bold">IDK</h1>
+
+    <div class="mt-5 flex flex-col items-center transition-all duration-300">
+        <a href="/" class={location === "/" ? "text-ctp-blue" : ""}>
+            <Home size={24} class="my-auto" />
+            <span class="ml-2 text-lg font-semibold" hidden={!expanded}
+                >Home</span
+            >
+        </a>
+        <a href="/recent" class={location === "/recent" ? "text-ctp-blue" : ""}>
+            <Globe2 size={24} class="my-auto" />
+            <span class="ml-2 text-lg font-semibold" hidden={!expanded}
+                >Recent</span
+            >
+        </a>
+        <!---
+        <a
+            href="/settings"
+            class={location === "/settings" ? "text-ctp-blue" : ""}
+        >
+            <Cog size={24} class="my-auto" />
+            <span class="ml-2 text-lg font-semibold" hidden={!expanded}
+                >Settings</span
+            >
+        </a>
+        --->
+    </div>
+
+    <button
+        class="mt-auto text-center transition-colors duration-500 hover:bg-ctp-crust p-1 rounded-md"
+        on:click={() => (expanded = !expanded)}
+    >
+        <Menu size={32} />
+    </button>
+</nav>
+
+<style>
+    a {
+        @apply p-2 w-full text-center flex transition-colors duration-500 hover:bg-ctp-crust rounded-md mb-1;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import SideBar from "$lib/components/SideBar.svelte";
+</script>
+
+<div class="flex h-screen w-full">
+    <SideBar />
+    <div class="p-5">content</div>
+</div>
